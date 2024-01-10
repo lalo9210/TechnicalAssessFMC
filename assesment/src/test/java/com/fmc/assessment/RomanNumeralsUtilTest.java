53defdaf32c1832e305ffa2ddd1f6847f18b6b4a
Eduardo Hernandez <eduardo.hernandez@nxp.com>
2024-01-10 15:36:50 -0600

    feat: test ware added to validate RomanNumeralsUtil.arabicToRoman convertion
    These tests are using the annotations provided by JUnit Jupiter and
    validate the inserted number is converted to the specified roman numeral
    
    - 1898 = MDCCCXCVIII
    - 3944 = MMMCMXLIV

--- assesment/src/test/java/com/fmc/assessment/RomanNumeralsUtilTest.java
+++ assesment/src/test/java/com/fmc/assessment/RomanNumeralsUtilTest.java
@@ -1,5 +1,11 @@
 package com.fmc.assessment;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import com.fmc.assessment.util.RomanNumeralsUtil;
+
 /**
  * 
  * Java class to validate functionality of RomanNumeralsUtil using unit tests
@@ -9,5 +15,25 @@ package com.fmc.assessment;
  *
  */
 class RomanNumeralsUtilTest {
+
+	@Test
+	void given1898Arabic_WhenConvertingToRoman_ThenReturnMDCCCXCVIII() {
+
+		int arabic1898 = 1898;
+
+		String result = RomanNumeralsUtil.arabicToRoman(arabic1898);
+
+		assertThat(result).isEqualTo("MDCCCXCVIII");
+	}
+	
+	@Test
+	void given3944Arabic_WhenConvertingToRoman_ThenReturnMMMCMXLIV() {
+
+		int arabic3944 = 3944;
+
+		String result = RomanNumeralsUtil.arabicToRoman(arabic3944);
+
+		assertThat(result).isEqualTo("MMMCMXLIV");
+	}
 
 }
