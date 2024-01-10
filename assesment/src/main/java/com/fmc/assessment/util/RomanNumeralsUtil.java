fae8660d46e89c650aa15648f3616bd6099d7f14
Eduardo Hernandez <eduardo.hernandez@nxp.com>
2024-01-10 15:22:26 -0600

    feat: util class is added
    At the moment we only know that we need to perform two dofferent
    conversions, so this initial class only contain the name of those
    functions that are going to be developed.

---
+++ assesment/src/main/java/com/fmc/assessment/util/RomanNumeralsUtil.java
@@ -0,0 +1,20 @@
+package com.fmc.assessment.util;
+
+/**
+ * Utility class that contains the methods to convert arabics numbers to roman
+ * numerals and viceversa.
+ * 
+ * @author EHG
+ * 
+ */
+public class RomanNumeralsUtil {
+
+	public static String arabicToRoman(int arabicN) {
+		return "";
+	}
+
+	public static int romanToArabic(String romanNum) {
+		return 0;
+	}
+
+}
