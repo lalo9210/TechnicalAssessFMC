c93fa9acb7c579d2a5a4beea22db1f77bd230759
Eduardo Hernandez <eduardo.hernandez@nxp.com>
2024-01-10 15:43:10 -0600

    feat: the function to convert from roman numerals to arabics is completed
    The new funtion romanToArabic has been added into the utility class. Tto
    create this function, some basic rules about Roman Numerals have been
    implemented, this information is going to be added into the README.
    Basically, the received string is going to be iterated and depending on
    the next symbol, the corresponfing values is added or subtracted from
    the total sum.

--- assesment/src/main/java/com/fmc/assessment/util/RomanNumeralsUtil.java
+++ assesment/src/main/java/com/fmc/assessment/util/RomanNumeralsUtil.java
@@ -1,6 +1,8 @@
 package com.fmc.assessment.util;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Utility class that contains the methods to convert arabics numbers to roman
@@ -10,6 +12,8 @@ import java.util.List;
  * 
  */
 public class RomanNumeralsUtil {
+
+	static Map<Character, Integer> romanNumeralMap = new HashMap<Character, Integer>();
 
 	public static String arabicToRoman(int arabicN) {
 		if ((arabicN <= 0) || (arabicN > 4000)) {
@@ -35,7 +39,25 @@ public class RomanNumeralsUtil {
 	}
 
 	public static int romanToArabic(String romanNum) {
-		return 0;
+		romanNumeralMap.put('I', 1);
+		romanNumeralMap.put('V', 5);
+		romanNumeralMap.put('X', 10);
+		romanNumeralMap.put('L', 50);
+		romanNumeralMap.put('C', 100);
+		romanNumeralMap.put('D', 500);
+		romanNumeralMap.put('M', 1000);
+
+		int result = 0;
+
+		for (int i = 0; i < romanNum.length(); i++) {
+			if (i < romanNum.length() - 1
+					&& romanNumeralMap.get(romanNum.charAt(i + 1)) > romanNumeralMap.get(romanNum.charAt(i)))
+				result -= romanNumeralMap.get(romanNum.charAt(i));
+			else
+				result += romanNumeralMap.get(romanNum.charAt(i));
+		}
+
+		return result;
 	}
 
 }
